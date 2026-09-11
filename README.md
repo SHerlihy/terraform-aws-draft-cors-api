de9afeb0afbf55de01bb695bb2b5f8209feddd21
Steven Herlihy <steven_herlihy@yahoo.com>
2026-09-11 11:55:55 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,1 @@
+# Draft API with CORS
--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.0, <2.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 6.0, <7.0"
-    }
-  }
-}
-
 resource "aws_api_gateway_rest_api" "default" {
   name           = var.api_name
 

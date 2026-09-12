5dbc029abe40696333d5eed67e88d6d7f868e73f
Steven Herlihy <steven_herlihy@yahoo.com>
2026-09-12 21:36:35 +0100

    fmt

--- output.tf
+++ output.tf
@@ -3,7 +3,7 @@ output "api_id" {
 }
 
 output "root_resource_id" {
-  value   = aws_api_gateway_rest_api.default.root_resource_id
+  value = aws_api_gateway_rest_api.default.root_resource_id
 }
 
 output "execution_arn" {

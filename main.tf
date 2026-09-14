e8637ea2d18dec49cc709008ad100c8a58221fdb
Steven Herlihy <steven_herlihy@yahoo.com>
2026-09-14 14:04:35 +0100

    add proxy resource so all paths cors

--- main.tf
+++ main.tf
@@ -8,16 +8,22 @@ resource "aws_api_gateway_rest_api" "default" {
   tags = var.tags
 }
 
+resource "aws_api_gateway_resource" "default" {
+  rest_api_id = aws_api_gateway_rest_api.default.id
+  parent_id   = aws_api_gateway_rest_api.default.root_resource_id
+  path_part   = "{proxy+}"
+}
+
 resource "aws_api_gateway_method" "default" {
   rest_api_id   = aws_api_gateway_rest_api.default.id
-  resource_id   = aws_api_gateway_rest_api.default.root_resource_id
+  resource_id   = aws_api_gateway_resource.default.id
   http_method   = "OPTIONS"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "default" {
   rest_api_id = aws_api_gateway_rest_api.default.id
-  resource_id = aws_api_gateway_rest_api.default.root_resource_id
+  resource_id = aws_api_gateway_resource.default.id
 
   http_method = aws_api_gateway_method.default.http_method
   type        = "MOCK"
@@ -29,7 +35,7 @@ resource "aws_api_gateway_integration" "default" {
 
 resource "aws_api_gateway_method_response" "default" {
   rest_api_id = aws_api_gateway_rest_api.default.id
-  resource_id = aws_api_gateway_rest_api.default.root_resource_id
+  resource_id = aws_api_gateway_resource.default.id
   http_method = aws_api_gateway_method.default.http_method
 
   status_code = "200"
@@ -46,7 +52,7 @@ resource "aws_api_gateway_integration_response" "default" {
   depends_on = [aws_api_gateway_integration.default]
 
   rest_api_id = aws_api_gateway_rest_api.default.id
-  resource_id = aws_api_gateway_rest_api.default.root_resource_id
+  resource_id = aws_api_gateway_resource.default.id
   http_method = aws_api_gateway_method.default.http_method
   status_code = aws_api_gateway_method_response.default.status_code
 

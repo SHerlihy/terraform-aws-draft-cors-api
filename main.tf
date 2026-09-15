a73bc742f9c8d93536b635a91e20ab51ade34a99
Steven Herlihy <steven_herlihy@yahoo.com>
2026-09-15 12:20:36 +0100

    ensure cors is application/json

--- main.tf
+++ main.tf
@@ -8,58 +8,66 @@ resource "aws_api_gateway_rest_api" "root" {
   tags = var.tags
 }
 
-resource "aws_api_gateway_resource" "proxy" {
+resource "aws_api_gateway_resource" "any" {
   rest_api_id = aws_api_gateway_rest_api.root.id
   parent_id   = aws_api_gateway_rest_api.root.root_resource_id
   path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_method" "cors" {
-  rest_api_id   = aws_api_gateway_rest_api.root.id
-  resource_id   = aws_api_gateway_resource.proxy.id
-  http_method   = "OPTIONS"
+resource "aws_api_gateway_method" "preflight" {
+  rest_api_id = aws_api_gateway_rest_api.root.id
+  resource_id = aws_api_gateway_resource.any.id
+  http_method = "OPTIONS"
+
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "cors" {
+resource "aws_api_gateway_integration" "preflight" {
   rest_api_id = aws_api_gateway_rest_api.root.id
-  resource_id = aws_api_gateway_resource.proxy.id
+  resource_id = aws_api_gateway_resource.any.id
+  http_method = aws_api_gateway_method.preflight.http_method
 
-  http_method = aws_api_gateway_method.cors.http_method
-  type        = "MOCK"
+  type = "MOCK"
+
+  passthrough_behavior = "NEVER"
 
   request_templates = {
     "application/json" = "{\"statusCode\": 200}"
   }
 }
 
-resource "aws_api_gateway_method_response" "cors" {
-  rest_api_id = aws_api_gateway_rest_api.root.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.cors.http_method
+resource "aws_api_gateway_method_response" "preflight" {
+  depends_on = [
+    aws_api_gateway_method.preflight,
+    aws_api_gateway_integration.preflight
+  ]
 
+  rest_api_id = aws_api_gateway_rest_api.root.id
+  resource_id = aws_api_gateway_resource.any.id
+  http_method = aws_api_gateway_method.preflight.http_method
   status_code = "200"
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
     "method.response.header.Access-Control-Allow-Origin"  = true
-    "method.response.header.Access-Control-Max-Age"       = true
   }
 }
 
-resource "aws_api_gateway_integration_response" "cors" {
-  depends_on = [aws_api_gateway_integration.cors]
+resource "aws_api_gateway_integration_response" "preflight" {
+  depends_on = [
+    aws_api_gateway_method.preflight,
+    aws_api_gateway_integration.preflight
+  ]
 
   rest_api_id = aws_api_gateway_rest_api.root.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.cors.http_method
-  status_code = aws_api_gateway_method_response.cors.status_code
+  resource_id = aws_api_gateway_resource.any.id
+  http_method = aws_api_gateway_method.preflight.http_method
+  status_code = aws_api_gateway_method_response.preflight.status_code
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'accept,cache-control,content-type,x-api-key'"
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,PATCH,DELETE,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'"
+    "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,HEAD,OPTIONS,PUT,POST,PATCH'",
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-    "method.response.header.Access-Control-Max-Age"       = "'86400'"
   }
 }
--- output.tf
+++ output.tf
@@ -9,7 +9,3 @@ output "root_resource_id" {
 output "execution_arn" {
   value = aws_api_gateway_rest_api.root.execution_arn
 }
-
-output "proxy_id" {
-  value = aws_api_gateway_resource.proxy.id
-}
